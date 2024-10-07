a188da3e32995f5dbfc0d0960d3de000d0018620
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2024-10-07 22:15:13 +0330

    adding ui features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Todo } from './pages/Todo';
 import { Layout } from './components/Layout'; // Ensure this path is correct
+import { Addtask } from './components/Addtask';
 
 function App() {
   return (
     <Layout>
       <Todo />
+      <Addtask/>
     </Layout>
   );
 }
