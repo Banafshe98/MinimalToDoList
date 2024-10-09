6820199d8794f1f51786fefc4c770a9d9922d565
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2024-10-09 20:47:57 +0330

    added last ui feature

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Layout = ({children}) => {
   return (
-    <div className="items-center justify-center p-32 bg-slate-900 flex flex-col gap-2 px-5 py-1 ">
+    <div className="items-center justify-center p-32 bg-slate-900 flex flex-col gap-2 px-5 py-1 h-screen w-screen ">
       {children}
     </div>
   );
