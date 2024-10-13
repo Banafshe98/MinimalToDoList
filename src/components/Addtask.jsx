b33cd01edb6bbd70cdea996f19a4f5dd3e90223d
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2024-10-13 22:45:55 +0330

    ui edit

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -3,13 +3,13 @@ import Add from "../assets/Add.svg"
 
 export const Addtask = () => {
   return (
-    <div className="flex flex-nowrap items-center bg-gray-300 rounded-full">
+    <div className="flex flex-nowrap items-center bg-gray-50 rounded-full">
       <input
         className="text-md bg-transparent h-14 pl-6 pr-2 border-0 outline-none flex-1  placeholder:text-slate-600  focus:outline-double"
         type="text"
         placeholder="Add your task"
       />
-      <button className="flex items-center justify-center border-none rounded-full bg-orange-500 w-28 h-14 text-white text-lg font-medium cursor-pointer">
+      <button className="flex items-center justify-center border-none rounded-full bg-lime-500 w-28 h-14 text-white text-lg font-medium cursor-pointer">
         <img src={Add}></img>
       </button>
     </div>
