17ac2ab623853bd6f8a79105ce69b4ec7a0385ea
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2024-11-01 17:18:42 +0330

    fixed adding task by enter (deleted AddBtn component)

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import AddBtn from "./AddBtn";
+import Add from "../assets/Add.svg";
 
 export const Addtask = ({ inputText, handleChange, addItem }) => {
   return (
-    <div className="flex flex-nowrap items-center bg-gray-50 rounded-full">
+    <form className="flex flex-nowrap items-center bg-gray-50 rounded-full">
       <input
         className="text-md font-bold bg-transparent h-14 pl-6 pr-2 border-0 outline-none flex-1 placeholder:text-slate-400 focus:outline-double"
         type="text"
@@ -11,7 +11,12 @@ export const Addtask = ({ inputText, handleChange, addItem }) => {
         value={inputText}
         onChange={handleChange}
       />
-      <AddBtn addItem={addItem} /> {/* Pass addItem to AddBtn */}
-    </div>
+      <button
+        onClick={addItem}
+        className="flex items-center justify-center border-none rounded-full bg-lime-500 w-28 h-14 text-white text-lg font-medium cursor-pointer"
+      >
+        <img src={Add} alt="Add" />
+      </button>
+    </form>
   );
 };
